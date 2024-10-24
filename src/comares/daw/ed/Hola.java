7680b352f5c169b5c6b9811ccf8a56aed2204cdd
manologomez123 <145774876+manologomez123@users.noreply.github.com>
2024-10-24 09:10:33 +0200

    Update Hola.java

--- src/comares/daw/ed/Hola.java
+++ src/comares/daw/ed/Hola.java
@@ -7,7 +7,7 @@ public class Hola {
 		System.out.println("Soy Javier Castillo Luque");
 		System.out.println("Hola mi nombre es Gabriel");
 		System.out.println("Hola mi nombre es Juan");
-		
+		System.out.println("Hola mi nombre es Manolo. Ya podéis descargar los cambios. No olvidéis documentar el proceso con capturas.");
 
 	}
 
