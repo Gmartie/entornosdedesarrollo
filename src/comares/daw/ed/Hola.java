c5c95de3194d26b8f83e59d9792fae9eb61651fe
Javier Castillo <jcasluq543@iesmarquesdecomares.org>
2024-10-24 08:42:37 +0200

    Commit de Javier

--- src/comares/daw/ed/Hola.java
+++ src/comares/daw/ed/Hola.java
@@ -3,7 +3,10 @@ package comares.daw.ed;
 public class Hola {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("Soy Javier Castillo Luque");
+		
+		
 
 	}
 
