e4cecb70332ae3c214457f5ccd4780b6c7b0ef43
Javier Castillo <jcasluq543@iesmarquesdecomares.org>
2024-10-25 08:47:28 +0200

    Cambio de linea

--- src/comares/daw/ed/Hola.java
+++ src/comares/daw/ed/Hola.java
@@ -8,7 +8,7 @@ public class Hola {
 		System.out.println("Hola mi nombre es Gabriel");
 		System.out.println("Hola mi nombre es Juan");
 		System.out.println("Hola mi nombre es Manolo. Ya podéis descargar los cambios. No olvidéis documentar el proceso con capturas.");
-
+		System.out.println("Cambio hecho por Javier");
 	}
 
 }
